7e68493ea0232eb1d1414697ef43a7a500cabcf7
jenseneducation-krishna-paruchuri <paruchurikris@gmail.com>
2020-03-09 15:20:07 +0100

    searchform.vue

---
+++ src/components/Gallery.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="gallery">
+        <ul>
+           <li>
+           </li>
+        </ul>    
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
